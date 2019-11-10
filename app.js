af44b900dc32db3bb60cce57e55beeb258b5b19a
amar PC <amarvadla27@gmail.com>
2019-11-10 14:14:47 +0530

    game development completed

--- app.js
+++ app.js
@@ -3,32 +3,71 @@ new Vue({
     data: {
         monsterHealth: 100,
         playerHealth: 100,
-        isGameRunning: false
+        isGameRunning: false,
+        turns: []
     },
     methods: {
         startGame: function () {
-            this.isGameRunning = true,
-                this.monsterHealth = 100,
-                this.playerHealth = 100
+            this.isGameRunning = true
+            this.monsterHealth = 100
+            this.playerHealth = 100
+            this.turns = []
         },
         attack: function () {
-
-            this.monsterHealth -= this.calculateDamage(3, 10)
+            let damage = this.calculateDamage(3, 10)
+            this.monsterHealth -= damage
+            if (this.checkWin())
+                return
+            this.turns.unshift({
+                isPlayer: true,
+                text: "player attacked monster " + damage
+            })
+            this.monsterAttacks()
+        },
+        specialAttack: function () {
+            let damage = this.calculateDamage(10, 20)
+            this.monsterHealth -= damage
             if (this.checkWin())
                 return
 
-            this.playerHealth -= this.calculateDamage(4, 15)
-            this.checkWin()
-        },
-        specialAttack: function () {
+            this.turns.unshift({
+                isPlayer: true,
+                text: "player special attack monster " + damage
+            })
+            this.monsterAttacks()
 
         }, heal: function () {
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10
+            } else {
+                this.playerHealth = 100
+            }
+
+            this.turns.unshift({
+                isPlayer: true,
+                text: "player heals 10"
+            })
+
+            this.monsterAttacks()
 
         }, giveUp: function () {
+            this.isGameRunning = false
+            this.turns = []
+        },
+        monsterAttacks: function () {
+            let damage = this.calculateDamage(4, 15)
+            this.playerHealth -= damage
+            this.checkWin()
 
-        }, calculateDamage: function (min, max) {
+            this.turns.unshift({
+                isPlayer: false,
+                text: "monster attacked player " + damage
+            })
+        },
+        calculateDamage: function (min, max) {
             return Math.max(Math.floor(Math.random() * max) + 1, min)
-        }, checkWin: function () {
+        },
+        checkWin: function () {
             if (this.monsterHealth <= 0) {
                 if (confirm('You won! new game ?')) {
                     this.startGame()
